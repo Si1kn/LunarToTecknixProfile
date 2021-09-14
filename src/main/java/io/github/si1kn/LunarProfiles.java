a9c5518693e5448fdd4afe6e8704243f98c357b8
Si1kn <si1kn521@gmail.com>
2021-09-14 15:08:16 +1000

    Fix it so it uses users name instead of mine

--- src/main/java/io/github/si1kn/LunarProfiles.java
+++ src/main/java/io/github/si1kn/LunarProfiles.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class LunarProfiles {
 
     public List<File> loadProfiles() {
-        File[] directories = new File("C:\\Users\\si1kn\\.lunarclient\\settings\\game").listFiles(File::isDirectory);
+
+
+        File[] directories = new File("C:\\Users\\" + System.getProperty("user.name") + "\\.lunarclient\\settings\\game").listFiles(File::isDirectory);
 
         if (directories != null)
             return new ArrayList<>(Arrays.asList(directories));
